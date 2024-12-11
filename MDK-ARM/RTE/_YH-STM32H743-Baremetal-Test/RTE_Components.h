edcee2cbaa5cc40ec1b3294d28e6b62cd6fd4b44
zaichenlinux <zaichenlinux@foxmail.com>
2024-12-11 10:36:42 +0800

    add MDK tool

---
+++ MDK-ARM/RTE/_YH-STM32H743-Baremetal-Test/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'YH-STM32H743-Baremetal-Test' 
+ * Target:  'YH-STM32H743-Baremetal-Test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
